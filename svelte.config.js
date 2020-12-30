c9dd81260d53c35ad2cd7cdd58e96d3bf57a23e8
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2020-12-30 23:09:17 +0100

    Framework setup
    svelte, typescript, sass, postcss, autoprefixer

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+const sveltePreprocess = require('svelte-preprocess');
+
+module.exports = {
+  preprocess: sveltePreprocess({
+    postcss: {
+      plugins: [
+        require('autoprefixer')
+      ]
+    },
+    sass: {
+      sync: true,
+      implementation: require('sass'),
+    },
+  }),
+};
