59be32e9f80ae2aad8bddd41dc08f6e75ad2b735
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2023-04-11 18:03:59 +0200

    Got Ui working

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+
+	import "@fontsource/quicksand";
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+ </script>
+
+<slot />
+
+
+<style>
+  :global(body) {
+    font-family: "Quicksand", sans-serif;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<h1>Let's get cracking bones!</h1>
+</div>
